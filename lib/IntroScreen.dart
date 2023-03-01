efacce3818eba061268a3397fbb129a7d8a2234a
AdoboPepper <jasper.calimbo@usjr.edu.ph>
2023-03-01 11:14:08 +0800

    Adds IntroScreen

---
+++ lib/IntroScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screens/main.dart';
+
+class IntroScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'IntroScreen.dart';
 
 void main(){
   runApp(MyApp());
@@ -11,7 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      "/" : (context) => IntroScreen(),
+     routes: {
+       "/" : (context) => IntroScreen(),
+     },
 
     );
   }
